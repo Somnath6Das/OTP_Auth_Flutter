b4d914374eb7a3ce1d64391a1f3382e71912ef41
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-04-20 19:59:29 +0530

    phone auth verify

---
+++ lib/Home.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello!\n You are Signin"),
+      ),
+    );
+  }
+}
